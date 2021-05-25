9f8f2c9945bbebf4dd2b6eadcc51e938ac9e034a
alfredolopez80 <alfredolopez80@gmail.com>
2021-05-24 20:43:59 -0400

    ADD: initial Unit Test main method ERC20

--- test/ERC20.exceptvest.test.ts
+++ test/ERC20.exceptvest.test.ts
@@ -47,4 +47,44 @@ describe("ERC20 Full Test", async () => {
 			});
 		});
 	});
+
+	//   ** Function / Methods Balanceof, Transfer, TransferFrom, Approve, IncreaseAllowance, DecreaseAllowance */
+	//   ** 2. Test Basic Method of Smart Contract : How it is working - Test Case */
+	//   ** t1. Getting Total Supply Value and verify is the same  Max Total Supply*/
+	//   ** t2. Getting Balance ofANY Accounts Value and verify is the same EXPECT VALUE */
+	//   ** t3. Testing Workflow Transfer*/
+	//   ** t4. Testing Workflow TransferFrom*/
+	//   ** t5. Testing Workflow Increase and Decrease Allowance */
+
+
+	it("2. Should the right value for Balance Of, Transfer, Transfer From, and Approve, etc", async () => {
+		const OmniToken = await ethers.getContractFactory("OmniTokenV1");
+		const omnitoken = await upgrades.deployProxy(OmniToken, ["Hello, OMN Token Ver 1"]);
+
+		await omnitoken.deployed();
+		// verify the Address
+		console.log("Omni Token deployed to:", omnitoken.address);
+		// Verify the balance of the Owner
+		console.log("Balance of the Owner: ", (await omnitoken.balanceOf(await accounts[0].getAddress())).toString(), "must be 638 million!!! in wei");
+		expect((await omnitoken.balanceOf(await accounts[0].getAddress())).toString()).to.be.equal('638888889000000000000000000');
+		console.log("Total Supply: ", (await omnitoken.totalSupply()).toString(), "must be 638 million!!! in wei");
+		expect(((await omnitoken.totalSupply()).toString())).to.be.equal('638888889000000000000000000');
+		describe('Verify Balance Of / Transfer', async () => {
+			it("1.1.- Verify the Name of the Token", async () => {
+				console.log("Name of The Token: ", (await omnitoken.name()).toString(), "=====> must be OMNI App");
+				expect(((await omnitoken.name()).toString())).to.be.equal('OMNI App');
+			});
+			it("1.2.- Verify the Tiker of the Token", async () => {
+				console.log("Name of The Token: ", (await omnitoken.symbol()).toString(), "====> must be OMN");
+				expect(((await omnitoken.symbol()).toString())).to.be.equal('OMN');
+			});
+			it("1.3.- Verify the Decimals of the Token", async () => {
+				console.log("Name of The Token: ", (await omnitoken.decimals()), "=====> must be 18");
+				expect((await omnitoken.decimals())).to.be.equal(18);
+			});
+		});
+	});
+
+
+
 });
