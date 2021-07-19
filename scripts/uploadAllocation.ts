353459ae2460734e2eb8ba3130a23b0eff0decd6
alfredolopez80 <alfredo@omni.ai>
2021-07-19 12:27:24 -0400

    ADD: update test allocation

---
+++ scripts/uploadAllocation.ts
@@ -0,0 +1,38 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import {run, ethers, upgrades} from 'hardhat';
+import "@nomiclabs/hardhat-ethers";
+
+const main = async () => {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+	await run("compile");
+
+	const accounts = await ethers.getSigners();
+
+	// Define Owner Address
+	const owner = accounts[0];
+	// Create Instance of the contract
+	const OmniToken = await ethers.getContractAt("OmniTokenV2", "0xaBc6790673a60b8A7f588450f59D2d256b1aeF7F", owner);
+	// check if the contract is instanced
+	console.log("OMNI Coin Address:", OmniToken.address);
+
+}
+
+  // We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+.then(() => process.exit(0))
+.catch(error => {
+  console.error(error);
+  process.exit(1);
+});
